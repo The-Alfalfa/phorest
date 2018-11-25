bde8af881650349120f626bb5162d8d1cdb60c5a
Javi <javi.sps@gmail.com>
2018-11-25 16:40:12 +0000

    First commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import 'jquery';
+import 'bootstrap';
+import './scss/custom.scss';
+import './js/scripts.js';
+
+import './images/logo.png';
+import './images/salon_image.jpg';
+import './images/shape.svg';
---
+++ src/js/scripts.js
@@ -0,0 +1,23 @@
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+
+$.ajax({
+    type: "GET",
+    dataType: "json",
+    url: "http://phorest-ob-staging.com/wp-json/wp/v2/posts/?categories=87&per_page=4",
+    cache: false,
+    success: function(data){
+       data.forEach(post => {
+           $("#blog").append(`
+                <div class="item d-flex flex-column justify-content-end">
+                    <div>
+                        <h3>${post.title.rendered}</h3>
+                        <p>${post.excerpt.rendered}</p>
+                        <a href="${post.link}" target="_blank" class="btn btn-secondary">Read Post</a>
+                    </div>
+                </div>
+            `);
+       });
+    }
+});
